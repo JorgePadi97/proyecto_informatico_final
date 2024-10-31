58674ce5b90f0dd9e2607adeb6a6527756c33818
espacioreal <118315922+espacioreal@users.noreply.github.com>
2024-10-30 23:59:38 -0300

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,7 +1,7 @@
 <?php
-
 include('../conexion.php');
 session_start();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -9,35 +9,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sql = "SELECT * FROM usuarios WHERE nombre = '$username'";
     $result = $conn->query($sql);
 
-    
-    //Verifico que me este trayendo un usuario
+    // Verifico que se obtenga un usuario
     if ($result->num_rows > 0) {
-
         $user = $result->fetch_assoc();
-        if (password_verify($password, $user['contraseña'])) 
-        {
-            //Estoy asignando un usuario a la sesion
+
+        // Verificación de la contraseña
+        if (password_verify($password, $user['contraseña'])) {
+            // Asignación de datos de usuario a la sesión
             $_SESSION['username'] = $username;
             $_SESSION['id'] = $user['id'];
-            $_SESSION['rol_id'] = $user['rol_id'];
-            //Si soy administrador lo llevo al dashboard de admin
-            if ($_SESSION['rol_id'] == 1) 
-            {
-                header("Location: ../index.php");
-                exit;
-            }
-                  //Si soy otro usuario lo llevo a un dashboard normal
-                  header("Location: ../index.php");
-                  exit;
+            $_SESSION['rol_id'] = $user['rol_id'];  // Guardamos el rol_id directamente
 
-        } else 
-        {
+            // Redirigimos a index.php sin condicional, el rol se verificará en index.php
+            header("Location: ../index.php");
+            exit;
+        } else {
             echo "<div class='alert alert-danger mt-3'>Contraseña incorrecta</div>";
         }
-    } else 
-    {
+    } else {
         echo "<div class='alert alert-danger mt-3'>Usuario no encontrado</div>";
     }
 }
-
 ?>
